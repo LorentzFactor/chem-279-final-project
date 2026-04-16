d5e905b6b17c7694a61dc1e8b5a372fe788e9c0b
LorentzFactor <cole.bouton@gmail.com>
2026-04-16 00:43:43 +0000

    Update gamma gradient computation to use symmetry for swaps

--- src/system_lib/cndo2_system.cpp
+++ src/system_lib/cndo2_system.cpp
@@ -229,14 +229,12 @@ namespace system_lib {
 
         for(size_t iatom = 0; iatom < atoms_.size(); ++iatom) {
             const auto& atom_i = atoms_.at(iatom);
-            for(size_t jatom = 0; jatom < atoms_.size(); ++jatom) {
-                // Skip case where iatom = jatom
-                if (iatom == jatom) continue;
-
+            for(size_t jatom = iatom+1; jatom < atoms_.size(); ++jatom) {
                 const auto& atom_j = atoms_.at(jatom);
                 arma::vec3 term = calculate_gamma_dRa(atom_i.get_atomic_orbitals().at(0), atom_j.get_atomic_orbitals().at(0));
                 for(size_t idim = 0; idim < 3; ++idim) {
                     dGamma(idim, iatom, jatom) = term(idim);
+                    dGamma(idim, jatom, iatom) = -term(idim);
                 }
             }
         }
