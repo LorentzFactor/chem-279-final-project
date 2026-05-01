93ba81584cdd33d002ef6036e2d643850d8674f4
achase206 <alexmchase@gmail.com>
2026-05-01 15:22:28 -0700

    implemented delta_E calc

--- src/nmr_calculator.cpp
+++ src/nmr_calculator.cpp
@@ -12,9 +12,9 @@
 #include <armadillo>
 #include <nlohmann/json.hpp>
 
-#include "system_lib/system_lib.h"
-#include "solver_lib/solver_lib.h"
 #include "nmr_lib/nmr_lib.h"
+#include "solver_lib/solver_lib.h"
+#include "system_lib/system_lib.h"
 
 namespace fs = std::filesystem;
 using json = nlohmann::json;
@@ -22,7 +22,8 @@ using json = nlohmann::json;
 int main(int argc, char **argv) {
   // check that a config file is supplied
   if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " path/to/molecule_config.json" << " path/to/reference_config.json" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " path/to/molecule_config.json"
+              << " path/to/reference_config.json" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -38,7 +39,8 @@ int main(int argc, char **argv) {
   // parse the reference molecule config file
   fs::path reference_config_file_path(argv[2]);
   if (!fs::exists(reference_config_file_path)) {
-    std::cerr << "Path: " << reference_config_file_path << " does not exist" << std::endl;
+    std::cerr << "Path: " << reference_config_file_path << " does not exist"
+              << std::endl;
     return EXIT_FAILURE;
   }
   std::ifstream reference_config_file(reference_config_file_path);
@@ -56,65 +58,118 @@ int main(int argc, char **argv) {
   int reference_num_beta_electrons = reference_config["num_beta_electrons"];
   double reference_delta_E = reference_config["delta_e"];
 
-  system_lib::CNDO2System main_sys = system_lib::CNDO2System::from_files(atoms_file_path, "./basis", num_alpha_electrons, num_beta_electrons);
+  system_lib::CNDO2System main_sys = system_lib::CNDO2System::from_files(
+      atoms_file_path, "./basis", num_alpha_electrons, num_beta_electrons);
   fixed_point::solve_cndo(main_sys);
+
+  CarbonGraph main_graph = nmr_lib::build_carbon_graph(main_sys);
+
+  // Debug print for carbon-carbon adjacency list
+  // std::cout << "Main molecule carbon adjacency:\n";
+  // for (size_t i = 0; i < main_graph.adj.size(); ++i) {
+  //   std::cout << "C_local " << i << " (atom "
+  //             << main_graph.carbon_local_to_atom_idx[i] << "): ";
+  //   for (size_t nbr : main_graph.adj[i]) {
+  //     std::cout << nbr << " ";
+  //   }
+  //   std::cout << "\n";
+  // }
+
+  // Debug for delta_E on each carbon for main
+  std::cout << "Delta_E for each carbon on main:\n";
+  for (size_t i = 0; i < main_sys.num_atoms(); ++i) {
+    if (main_sys.get_atom(i).get_atomic_number() == 6) {
+      const size_t local_atom_idx = main_graph.atom_idx_to_carbon_local.at(i);
+      double delta_e = nmr_lib::calculate_delta_E(main_graph, i);
+      std::cout << "Carbon " << local_atom_idx << " delta_E: " << delta_e
+                << '\n';
+    }
+  }
 
   // compute sigma for each carbon atom and print it
   int num_carbons_main = 0;
   for (size_t iatom = 0; iatom < main_sys.num_atoms(); iatom++) {
-      const auto& atom = main_sys.get_atom(iatom);
-      if (atom.get_symbol() == "C") {
-          ++num_carbons_main;
-          double sigma_d = nmr_lib::calculate_sigma_d(main_sys, iatom);
-          double sigma_p = nmr_lib::calculate_sigma_p(main_sys, iatom, delta_E);
-          double sigma = nmr_lib::calculate_sigma(main_sys, iatom, delta_E);
-          std::cout << std::format("Atom {}: sigma_d = {:.4f}, sigma_p = {:.4f}, sigma = {:.4f}\n", iatom, sigma_d, sigma_p, sigma);
-      }
+    const auto &atom = main_sys.get_atom(iatom);
+    if (atom.get_symbol() == "C") {
+      ++num_carbons_main;
+      double sigma_d = nmr_lib::calculate_sigma_d(main_sys, iatom);
+      double sigma_p = nmr_lib::calculate_sigma_p(main_sys, main_graph, iatom);
+      double sigma = nmr_lib::calculate_sigma(main_sys, main_graph, iatom);
+      std::cout << std::format(
+          "Atom {}: sigma_d = {:.4f}, sigma_p = {:.4f}, sigma = {:.4f}\n",
+          iatom, sigma_d, sigma_p, sigma);
+    }
   }
 
-  system_lib::CNDO2System reference_sys = system_lib::CNDO2System::from_files(reference_atoms_file_path, "./basis", reference_num_alpha_electrons, reference_num_beta_electrons);
+  system_lib::CNDO2System reference_sys = system_lib::CNDO2System::from_files(
+      reference_atoms_file_path, "./basis", reference_num_alpha_electrons,
+      reference_num_beta_electrons);
   fixed_point::solve_cndo(reference_sys);
+  CarbonGraph ref_graph = nmr_lib::build_carbon_graph(reference_sys);
+
+  // Debug for delta_E on each carbon for reference
+  std::cout << "Delta_E for each carbon on reference:\n";
+  for (size_t i = 0; i < reference_sys.num_atoms(); ++i) {
+    if (reference_sys.get_atom(i).get_atomic_number() == 6) {
+      const size_t local_atom_idx = ref_graph.atom_idx_to_carbon_local.at(i);
+      double delta_e = nmr_lib::calculate_delta_E(ref_graph, i);
+      std::cout << "Carbon " << local_atom_idx << " delta_E: " << delta_e
+                << '\n';
+    }
+  }
 
   // compute sigma for each carbon atom and print it
   int num_carbons_reference = 0;
   for (size_t iatom = 0; iatom < reference_sys.num_atoms(); iatom++) {
-      const auto& atom = reference_sys.get_atom(iatom);
-      if (atom.get_symbol() == "C") {
-          num_carbons_reference++;
-          double sigma_d = nmr_lib::calculate_sigma_d(reference_sys, iatom);
-          double sigma_p = nmr_lib::calculate_sigma_p(reference_sys, iatom, reference_delta_E);
-          double sigma = nmr_lib::calculate_sigma(reference_sys, iatom, reference_delta_E);
-          std::cout << std::format("Reference Atom {}: sigma_d = {:.4f}, sigma_p = {:.4f}, sigma = {:.4f}\n", iatom, sigma_d, sigma_p, sigma);
-      }
+    const auto &atom = reference_sys.get_atom(iatom);
+    if (atom.get_symbol() == "C") {
+      num_carbons_reference++;
+      double sigma_d = nmr_lib::calculate_sigma_d(reference_sys, iatom);
+      double sigma_p =
+          nmr_lib::calculate_sigma_p(reference_sys, ref_graph, iatom);
+      double sigma = nmr_lib::calculate_sigma(reference_sys, ref_graph, iatom);
+      std::cout << std::format("Reference Atom {}: sigma_d = {:.4f}, sigma_p = "
+                               "{:.4f}, sigma = {:.4f}\n",
+                               iatom, sigma_d, sigma_p, sigma);
+    }
   }
 
-  arma::mat chemical_shifts = arma::zeros(num_carbons_main, num_carbons_reference);
+  arma::mat chemical_shifts =
+      arma::zeros(num_carbons_main, num_carbons_reference);
 
-  for (size_t iatom_main = 0, iC_main = 0; iatom_main < main_sys.num_atoms(); iatom_main++) {
-      const auto& atom_main = main_sys.get_atom(iatom_main);
-      if (atom_main.get_symbol() != "C") continue;
+  for (size_t iatom_main = 0, iC_main = 0; iatom_main < main_sys.num_atoms();
+       iatom_main++) {
+    const auto &atom_main = main_sys.get_atom(iatom_main);
+    if (atom_main.get_symbol() != "C")
+      continue;
 
-      double sigma_main = nmr_lib::calculate_sigma(main_sys, iatom_main, delta_E);
+    double sigma_main =
+        nmr_lib::calculate_sigma(main_sys, main_graph, iatom_main);
 
-      for (size_t iatom_ref = 0, iC_ref = 0; iatom_ref < reference_sys.num_atoms(); iatom_ref++) {
-          const auto& atom_ref = reference_sys.get_atom(iatom_ref);
-          if (atom_ref.get_symbol() != "C") continue;
+    // delta_c for reference -> currently not using!!!
+    const double delta_c = 0.0;
+    for (size_t iatom_ref = 0, iC_ref = 0;
+         iatom_ref < reference_sys.num_atoms(); iatom_ref++) {
+      const auto &atom_ref = reference_sys.get_atom(iatom_ref);
+      if (atom_ref.get_symbol() != "C")
+        continue;
 
-          double sigma_ref = nmr_lib::calculate_sigma(reference_sys, iatom_ref, reference_delta_E);
-          chemical_shifts(iC_main, iC_ref) = sigma_ref - sigma_main;
-          ++iC_ref;
-      }
-      ++iC_main;
+      double sigma_ref =
+          nmr_lib::calculate_sigma(reference_sys, ref_graph, iatom_ref);
+      chemical_shifts(iC_main, iC_ref) = (sigma_ref - sigma_main) + delta_c;
+      ++iC_ref;
+    }
+    ++iC_main;
   }
-
 
   // Print the chemical shifts
   std::cout << "Chemical Shifts (ppm):\n";
   for (size_t i = 0; i < chemical_shifts.n_rows; i++) {
-      for (size_t j = 0; j < chemical_shifts.n_cols; j++) {
-          std::cout << std::format("Main Atom {} vs Reference Atom {}: {:.4f} ", i, j, chemical_shifts(i, j)) << "ppm; \n";
-      }
+    for (size_t j = 0; j < chemical_shifts.n_cols; j++) {
+      std::cout << std::format("Main Atom {} vs Reference Atom {}: {:.4f} ", i,
+                               j, chemical_shifts(i, j))
+                << "ppm; \n";
+    }
     std::cout << "\n";
   }
-  
 }
--- src/nmr_lib/nmr_lib.cpp
+++ src/nmr_lib/nmr_lib.cpp
@@ -1,95 +1,294 @@
 #include "nmr_lib.h"
+#include <cstddef>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
 
 namespace nmr_lib {
-    double calculate_sigma_d(const CNDO2System& system, const size_t atom_A_idx) {
-        const Atom& atom_A = system.get_atom(atom_A_idx);
-        if (atom_A.get_symbol() != "C") {
-            throw std::runtime_error("Only carbon atoms are supported for sigma_d calculation.");
-        }
+double calculate_sigma_d(const CNDO2System &system, const size_t atom_A_idx) {
+  const Atom &atom_A = system.get_atom(atom_A_idx);
+  if (atom_A.get_symbol() != "C") {
+    throw std::runtime_error(
+        "Only carbon atoms are supported for sigma_d calculation.");
+  }
 
-        /* Calculate q - the effective electron density surrounding atom A.
-        *  We add 2 for the 2 electrons in the 1s orbital of the (carbon) itself,
-        *  which are not included in the density matrix but do contribute to shielding.
-        */
-        double q = system.get_electron_density(atom_A_idx) + 2;
-        // constants for z_star given by Takaishi 1974
-        double z_star = 3.25 - 0.35 * (q-4);
-        double sigma_d = 4.45*z_star*q;
-        return sigma_d;
-    }
-
-    double get_system_orbital_idx_by_name(const CNDO2System& system, size_t atom_idx, const std::string& orbital_name) {
-        const Atom& atom = system.get_atom(atom_idx);
-        const auto& orbital_names = atom.get_orbital_names();
-        const auto& orbital_idxs = system.get_atom_orbital_idxs().at(atom_idx);
-        for (size_t i = 0; i < orbital_names.size(); i++) {
-            if (orbital_names.at(i) == orbital_name) {
-                return orbital_idxs[0] + i;
-            }
-        }
-        throw std::runtime_error("Orbital name not found for atom " + atom.get_symbol() + ": " + orbital_name);
-    }
-
-    double calculate_q2p(const CNDO2System& system, size_t atom_A_idx) {
-        const Atom& atom_A = system.get_atom(atom_A_idx);
-        arma::mat P = system.get_p_alpha() + system.get_p_beta();
-
-        double q2p = 0;
-        for(auto& dim : std::array<std::string, 3>{"x", "y", "z"}) {
-            double idx_A = get_system_orbital_idx_by_name(system, atom_A_idx, "p"+dim);
-            q2p += P(idx_A, idx_A);
-        }
-        return q2p;
-     }
-
-    /* See equation 8 of the paper */
-    double calculate_Q_AB(const CNDO2System& system, const size_t atom_A_idx, const size_t atom_B_idx, double q2p) {
-        const Atom& atom_A = system.get_atom(atom_A_idx);
-        const Atom& atom_B = system.get_atom(atom_B_idx);
-
-        const arma::mat& P = system.get_p_alpha() + system.get_p_beta();
-
-        double Q_AB = 0.0;
-        // First term
-        if (atom_A_idx == atom_B_idx) {
-            Q_AB += 4.0 / 3.0 * q2p;
-        }
-
-        std::array<std::string, 3> dims = {"x", "y", "z"};
-
-        // Compute second & third terms together
-        for(size_t idim = 0; idim < 3; ++idim) {
-            double idx_A = get_system_orbital_idx_by_name(system, atom_A_idx, "p"+dims[idim]);
-            double idx_B = get_system_orbital_idx_by_name(system, atom_B_idx, "p"+dims[idim]);
-            for (size_t jdim = idim+1; jdim < 3; ++jdim) {
-                double idx_A_j = get_system_orbital_idx_by_name(system, atom_A_idx, "p"+dims[jdim]);
-                double idx_B_j = get_system_orbital_idx_by_name(system, atom_B_idx, "p"+dims[jdim]);
-                Q_AB -= 2.0/3.0 * P(idx_A, idx_B) * P(idx_A_j, idx_B_j);
-                Q_AB += 2.0/3.0 * P(idx_A, idx_B_j) * P(idx_A_j, idx_B);
-            }
-        }
-        return Q_AB;
-     }
-
-    double calculate_sigma_p(const CNDO2System& system, const size_t atom_A_idx, double delta_E) {
-        double q2p = calculate_q2p(system, atom_A_idx);
-        
-        double sigma_p = 0;
-        for(size_t jatom = 0; jatom < system.num_atoms(); ++jatom) {
-            // cb - Open question - should this skip the atom itself? Takaishi's paper doesn't specify.
-            // Raw formula wold include the atom itself...
-            //if (jatom == atom_A_idx) continue;
-            if (system.get_atom(jatom).get_atomic_number() < 3) continue;
-            double Q_AB = calculate_Q_AB(system, atom_A_idx, jatom, q2p);
-            sigma_p += Q_AB;
-        }
-        double r3_2p = 1/(24*BOHR_RADIUS*BOHR_RADIUS*BOHR_RADIUS)*std::pow(3.25 - 0.35 * (q2p-3), 3);
-        //double delta_E = calculate_delta_E(system);
-
-        // e^2*h_bar^2/(2m^2*c^2 \delta E)
-        double prefactor = 1.0736e2; // scaled to ppm by 1e6, assumes distances are in angstroms and delta_E is in eV
-    
-        sigma_p *= r3_2p * prefactor / delta_E;
-        return -sigma_p;
-    }
+  /* Calculate q - the effective electron density surrounding atom A.
+   *  We add 2 for the 2 electrons in the 1s orbital of the (carbon) itself,
+   *  which are not included in the density matrix but do contribute to
+   * shielding.
+   */
+  double q = system.get_electron_density(atom_A_idx) + 2;
+  // constants for z_star given by Takaishi 1974
+  double z_star = 3.25 - 0.35 * (q - 4);
+  double sigma_d = 4.45 * z_star * q;
+  return sigma_d;
 }
+
+double get_system_orbital_idx_by_name(const CNDO2System &system,
+                                      size_t atom_idx,
+                                      const std::string &orbital_name) {
+  const Atom &atom = system.get_atom(atom_idx);
+  const auto &orbital_names = atom.get_orbital_names();
+  const auto &orbital_idxs = system.get_atom_orbital_idxs().at(atom_idx);
+  for (size_t i = 0; i < orbital_names.size(); i++) {
+    if (orbital_names.at(i) == orbital_name) {
+      return orbital_idxs[0] + i;
+    }
+  }
+  throw std::runtime_error("Orbital name not found for atom " +
+                           atom.get_symbol() + ": " + orbital_name);
+}
+
+double calculate_q2p(const CNDO2System &system, size_t atom_A_idx) {
+  const Atom &atom_A = system.get_atom(atom_A_idx);
+  arma::mat P = system.get_p_alpha() + system.get_p_beta();
+
+  double q2p = 0;
+  for (auto &dim : std::array<std::string, 3>{"x", "y", "z"}) {
+    double idx_A =
+        get_system_orbital_idx_by_name(system, atom_A_idx, "p" + dim);
+    q2p += P(idx_A, idx_A);
+  }
+  return q2p;
+}
+
+/* See equation 8 of the paper */
+double calculate_Q_AB(const CNDO2System &system, const size_t atom_A_idx,
+                      const size_t atom_B_idx, double q2p) {
+  const Atom &atom_A = system.get_atom(atom_A_idx);
+  const Atom &atom_B = system.get_atom(atom_B_idx);
+
+  const arma::mat &P = system.get_p_alpha() + system.get_p_beta();
+
+  double Q_AB = 0.0;
+  // First term
+  if (atom_A_idx == atom_B_idx) {
+    Q_AB += 4.0 / 3.0 * q2p;
+  }
+
+  std::array<std::string, 3> dims = {"x", "y", "z"};
+
+  // Compute second & third terms together
+  for (size_t idim = 0; idim < 3; ++idim) {
+    double idx_A =
+        get_system_orbital_idx_by_name(system, atom_A_idx, "p" + dims[idim]);
+    double idx_B =
+        get_system_orbital_idx_by_name(system, atom_B_idx, "p" + dims[idim]);
+    for (size_t jdim = idim + 1; jdim < 3; ++jdim) {
+      double idx_A_j =
+          get_system_orbital_idx_by_name(system, atom_A_idx, "p" + dims[jdim]);
+      double idx_B_j =
+          get_system_orbital_idx_by_name(system, atom_B_idx, "p" + dims[jdim]);
+      Q_AB -= 2.0 / 3.0 * P(idx_A, idx_B) * P(idx_A_j, idx_B_j);
+      Q_AB += 2.0 / 3.0 * P(idx_A, idx_B_j) * P(idx_A_j, idx_B);
+    }
+  }
+  return Q_AB;
+}
+
+double calculate_sigma_p(const CNDO2System &system, const CarbonGraph &graph,
+                         const size_t atom_A_idx) {
+  double q2p = calculate_q2p(system, atom_A_idx);
+
+  double sigma_p = 0;
+  for (size_t jatom = 0; jatom < system.num_atoms(); ++jatom) {
+    // cb - Open question - should this skip the atom itself? Takaishi's paper
+    // doesn't specify. Raw formula wold include the atom itself...
+    // if (jatom == atom_A_idx) continue;
+    if (system.get_atom(jatom).get_atomic_number() < 3)
+      continue;
+    double Q_AB = calculate_Q_AB(system, atom_A_idx, jatom, q2p);
+    sigma_p += Q_AB;
+  }
+  double r3_2p = 1 / (24 * BOHR_RADIUS * BOHR_RADIUS * BOHR_RADIUS) *
+                 std::pow(3.25 - 0.35 * (q2p - 3), 3);
+
+  double delta_E = calculate_delta_E(graph, atom_A_idx);
+
+  // e^2*h_bar^2/(2m^2*c^2 \delta E)
+  double prefactor = 1.0736e2; // scaled to ppm by 1e6, assumes distances are in
+                               // angstroms and delta_E is in eV
+
+  sigma_p *= r3_2p * prefactor / delta_E;
+
+  // debugging stuff
+  //   const size_t local_carbon_idx =
+  //   graph.atom_idx_to_carbon_local.at(atom_A_idx); AlphaBetaGammaCounts
+  //   counts = count_alpha_beta_gamma(graph, local_carbon_idx); int a =
+  //   counts.a; int b = counts.b; int c = counts.c; std::cout << "Carbon " <<
+  //   local_carbon_idx << "-> a=" << a << ", b=" << b
+  //             << ", c=" << c << ", dE=" << delta_E << " sig_p=" << -sigma_p
+  //             << '\n';
+
+  return -sigma_p;
+}
+
+double calculate_delta_E(const CarbonGraph &graph, const size_t atom_A_idx) {
+  const size_t local_atom_A_idx = graph.atom_idx_to_carbon_local.at(atom_A_idx);
+  AlphaBetaGammaCounts counts = count_alpha_beta_gamma(graph, local_atom_A_idx);
+  SMotifCounts motifs = count_motifs(graph, local_atom_A_idx);
+  double a = pow(1.015, counts.a);
+  double b = pow(0.958, counts.b);
+  double c = pow(1.010, counts.c);
+
+  double term_A = 11.30 * a * b * c;
+
+  double d = pow(1.011, motifs.n_1_4);
+  double e = pow(1.009, motifs.n_2_3);
+  double f = pow(1.029, motifs.n_2_4);
+  double g = pow(1.009, motifs.n_3_2);
+  double h = pow(1.030, motifs.n_3_3);
+  double i = pow(1.031, motifs.n_4_2);
+
+  double term_S = d * e * f * g * h * i;
+
+  return term_A * term_S;
+}
+
+CarbonGraph build_carbon_graph(const CNDO2System &system,
+                               double cc_cutoff_bohr) {
+  CarbonGraph graph;
+
+  // Build the carbon idx to global idx mapping
+  for (size_t global_idx = 0; global_idx < system.num_atoms(); ++global_idx) {
+    if (system.get_atom(global_idx).get_atomic_number() == 6) {
+      size_t local_idx = graph.carbon_local_to_atom_idx.size();
+      graph.carbon_local_to_atom_idx.push_back(global_idx);
+      graph.atom_idx_to_carbon_local[global_idx] = local_idx;
+    }
+  }
+
+  // Build the carbon-carbon adjacency list
+  size_t num_carbons = graph.carbon_local_to_atom_idx.size();
+  graph.adj.resize(num_carbons, std::vector<size_t>(0, 0));
+
+  for (size_t idxA = 0; idxA < num_carbons; ++idxA) {
+    for (size_t idxB = idxA + 1; idxB < num_carbons; ++idxB) {
+      size_t carbA = graph.carbon_local_to_atom_idx.at(idxA);
+      size_t carbB = graph.carbon_local_to_atom_idx.at(idxB);
+      const Atom &atomA = system.get_atom(carbA);
+      const Atom &atomB = system.get_atom(carbB);
+      double dist = distance(atomA, atomB);
+      if (dist <= cc_cutoff_bohr) {
+        graph.adj[idxA].push_back(idxB);
+        graph.adj[idxB].push_back(idxA);
+      }
+    }
+  }
+
+  return graph;
+}
+
+AlphaBetaGammaCounts count_alpha_beta_gamma(const CarbonGraph &graph,
+                                            size_t local_idx) {
+  AlphaBetaGammaCounts counts;
+
+  const size_t n = graph.adj.size();
+  std::vector<int> dist(n, -1);
+  std::queue<size_t> q;
+
+  dist[local_idx] = 0;
+  q.push(local_idx);
+
+  // Perform BFS to get counts on neighboring carbons
+  while (!q.empty()) {
+    size_t u = q.front();
+    q.pop();
+
+    // Only search up to depth 3 (a=1, b=2 and c=3)
+    if (dist[u] >= 3)
+      continue;
+
+    for (size_t v : graph.adj[u]) {
+      if (dist[v] != -1)
+        continue; // visited already
+      dist[v] = dist[u] + 1;
+
+      if (dist[v] == 1)
+        counts.a++;
+      else if (dist[v] == 2)
+        counts.b++;
+      else if (dist[v] == 3)
+        counts.c++;
+
+      q.push(v);
+    }
+  }
+
+  return counts;
+}
+
+SMotifCounts count_motifs(const CarbonGraph &graph, size_t local_idx) {
+  SMotifCounts m{};
+
+  // BFS distances from target carbon
+  const size_t n = graph.adj.size();
+  std::vector<int> dist(n, -1);
+  std::queue<size_t> q;
+  dist[local_idx] = 0;
+  q.push(local_idx);
+
+  while (!q.empty()) {
+    size_t u = q.front();
+    q.pop();
+    if (dist[u] >= 3)
+      continue;
+
+    for (size_t v : graph.adj[u]) {
+      if (dist[v] != -1)
+        continue;
+      dist[v] = dist[u] + 1;
+      q.push(v);
+    }
+  }
+
+  // Lambda for getting the carbon class based on n neighbors in adj list
+  auto carbon_class = [&](size_t local) {
+    int deg = static_cast<int>(graph.adj.at(local).size());
+    if (deg <= 1)
+      return 1;
+    if (deg == 2)
+      return 2;
+    if (deg == 3)
+      return 3;
+    return 4;
+  };
+
+  for (size_t u = 0; u < n; ++u) {
+    if (dist[u] == -1 || dist[u] > 3)
+      continue;
+    int cu = carbon_class(u);
+
+    for (size_t v : graph.adj.at(u)) {
+      if (u >= v)
+        continue; // only count edges once
+      if (dist[v] == -1 || dist[v] > 3)
+        continue;
+      int cv = carbon_class(v);
+
+      // Count ordered motifs
+      // [cu(cv)] and [cv(cu)]
+      auto add_ordered = [&](int a, int b) {
+        if (a == 1 && b == 4)
+          m.n_1_4++;
+        if (a == 2 && b == 3)
+          m.n_2_3++;
+        if (a == 2 && b == 4)
+          m.n_2_4++;
+        if (a == 3 && b == 2)
+          m.n_3_2++;
+        if (a == 3 && b == 3)
+          m.n_3_3++;
+        if (a == 4 && b == 2)
+          m.n_4_2++;
+      };
+
+      add_ordered(cu, cv);
+      add_ordered(cv, cu);
+    }
+  }
+  return m;
+}
+
+} // namespace nmr_lib
